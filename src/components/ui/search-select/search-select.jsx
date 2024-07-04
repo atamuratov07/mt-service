84d5508aa2600cc4bc8a98329a8cba64340c4c8c
Damir <damiratamuratov07@gmail.com>
2024-07-04 12:01:55 +0500

    changed select header color

--- src/components/ui/search-select/search-select.jsx
+++ src/components/ui/search-select/search-select.jsx
@@ -24,7 +24,7 @@ const defaultClasses = {
 	selected: '!bg-accent text-ternary',
 	item: '',
 	group: 'min-h-max',
-	groupHeader: 'text-gray-500 font-medium',
+	groupHeader: 'text-accent font-medium',
 	groupList: '',
 }
 
